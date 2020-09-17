80d561991301a07d7f805663c6369ec3578279af
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2020-09-17 09:54:12 +0800

    Add cmake files

--- Chapter04/main.cpp
+++ Chapter04/main.cpp
@@ -5,14 +5,13 @@
 #include "recipe_4_05.h"
 #include "recipe_4_06.h"
 
-int main()
-{
-   recipe_4_01::execute();
-   recipe_4_02::execute();
-   recipe_4_03::execute();
-   recipe_4_04::execute();
-   recipe_4_05::execute();
-   recipe_4_06::execute();
+int main() {
+    recipe_4_01::execute();
+    recipe_4_02::execute();
+    recipe_4_03::execute();
+    recipe_4_04::execute();
+    recipe_4_05::execute();
+    recipe_4_06::execute();
 
-   return 0;
+    return 0;
 }
--- Chapter04/recipe_4_01.h
+++ Chapter04/recipe_4_01.h
@@ -2,122 +2,114 @@
 
 #include <iostream>
 
-namespace recipe_4_01
-{
+namespace recipe_4_01 {
 
 #if !defined(_UNIQUE_NAME_)
 #define _UNIQUE_NAME_
 
-   class foo
-   {
-   };
+class foo {};
 
 #endif
 
-   void show_compiler()
-   {
+void show_compiler() {
 #if defined _MSC_VER
-      std::cout << "Visual C++" << std::endl;
+    std::cout << "Visual C++" << std::endl;
 #elif defined __clang__
-      std::cout << "Clang" << std::endl;
+    std::cout << "Clang" << std::endl;
 #elif defined __GNUG__
-      std::cout << "GCC" << std::endl;
+    std::cout << "GCC" << std::endl;
 #else
-      std::cout << "Unknown compiler" << std::endl;
+    std::cout << "Unknown compiler" << std::endl;
 #endif
-   }
+}
 
-   void show_compiler_version()
-   {
+void show_compiler_version() {
 #if defined _MSC_VER
 
-      std::cout <<
+    std::cout <<
 #if (_MSC_VER >= 1900)
-         "Visual C++ 2015 or newer"
-#else 
-         "Visual C++ " << _MSC_FULL_VER
+        "Visual C++ 2015 or newer"
+#else
+        "Visual C++ " << _MSC_FULL_VER
 #endif
-         << std::endl;
+              << std::endl;
 
 #elif defined __clang__
 
-      std::cout <<
+    std::cout <<
 #if (__clang_major__ == 3) && (__clang_minor__ >= 9)
-         "Clang 3.9 or newer"
+        "Clang 3.9 or newer"
 #else
-         "Clang " << __clang_version__
+        "Clang " << __clang_version__
 #endif
-         << std::endl;
+              << std::endl;
 
 #elif defined __GNUG__
 
-      std::cout <<
+    std::cout <<
 #if __GNUC__ >= 5 && __GNUC_MINOR__ > 0
-         "GCC 5.0 or newer"
+        "GCC 5.0 or newer"
 #else
-         "GCC " << __VERSION__
+        "GCC " << __VERSION__
 #endif
-         << std::endl;
+              << std::endl;
 
 #else
-      std::cout << "Unknown compiler" << std::endl;
+    std::cout << "Unknown compiler" << std::endl;
 #endif
-   }
+}
 
-   void show_architecture()
-   {
+void show_architecture() {
 #if defined _MSC_VER
 
-      std::cout <<
+    std::cout <<
 #if defined _M_X64
-         "AMD64"
+        "AMD64"
 #elif defined _M_IX86
-         "INTEL x86"
+        "INTEL x86"
 #elif defined _M_ARM
-         "ARM"
+        "ARM"
 #else
-         "unknown"
+        "unknown"
 #endif
-         << std::endl;
+              << std::endl;
 
 #elif defined __clang__ || __GNUG__
 
-      std::cout <<
+    std::cout <<
 #if defined __amd64__
-         "AMD64"
+        "AMD64"
 #elif defined __i386__
-         "INTEL x86"
+        "INTEL x86"
 #elif defined __arm__
-         "ARM"
+        "ARM"
 #else
-         "unknown"
+        "unknown"
 #endif
-         << std::endl;
+              << std::endl;
 
 #else
 #error Unknown compiler
 #endif
-   }
-
-   void show_configuration()
-   {
-      std::cout <<
-#ifdef _DEBUG
-         "debug"
-#else
-         "release"
-#endif
-         << std::endl;
-   }
-
-   void execute()
-   {
-      show_compiler();
-
-      show_compiler_version();
-
-      show_architecture();
-
-      show_configuration();
-   }
 }
+
+void show_configuration() {
+    std::cout <<
+#ifdef _DEBUG
+        "debug"
+#else
+        "release"
+#endif
+              << std::endl;
+}
+
+void execute() {
+    show_compiler();
+
+    show_compiler_version();
+
+    show_architecture();
+
+    show_configuration();
+}
+} // namespace recipe_4_01
