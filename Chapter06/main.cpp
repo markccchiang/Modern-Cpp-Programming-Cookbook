03eea3e5ef269c0a479407252649e86a15ed0864
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2020-09-17 10:48:47 +0800

    Add cmake files

--- Chapter06/main.cpp
+++ Chapter06/main.cpp
@@ -5,24 +5,23 @@
 #include "recipe_6_05.h"
 #include "recipe_6_06.h"
 #include "recipe_6_07.h"
-#include "recipe_6_08.h"
+//#include "recipe_6_08.h"
 #include "recipe_6_09.h"
 #include "recipe_6_10.h"
 #include "recipe_6_11.h"
 
-int main()
-{
-   recipe_6_01::execute();
-   recipe_6_02::execute();
-   recipe_6_03::execute();
-   recipe_6_04::execute();
-   recipe_6_05::execute();
-   recipe_6_06::execute();
-   recipe_6_07::execute();
-   recipe_6_08::execute();
-   recipe_6_09::execute();
-   recipe_6_10::execute();
-   recipe_6_11::execute();
+int main() {
+    recipe_6_01::execute();
+    recipe_6_02::execute();
+    recipe_6_03::execute();
+    recipe_6_04::execute();
+    recipe_6_05::execute();
+    recipe_6_06::execute();
+    recipe_6_07::execute();
+    // recipe_6_08::execute();
+    recipe_6_09::execute();
+    recipe_6_10::execute();
+    recipe_6_11::execute();
 
-   return 0;
+    return 0;
 }
