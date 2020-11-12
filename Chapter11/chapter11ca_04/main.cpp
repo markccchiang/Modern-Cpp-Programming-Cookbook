26b499ddf948f305f67ef67ca9e6547445ed0606
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2020-11-12 20:45:35 +0800

    Add cmake files

--- Chapter11/chapter11ca_04/main.cpp
+++ Chapter11/chapter11ca_04/main.cpp
@@ -1,18 +1,14 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include <catch2/catch.hpp>
 
-TEST_CASE("case1")
-{
-   SECTION("function1")
-   {
-      REQUIRE(true);
-   }
+TEST_CASE("case1") {
+    SECTION("function1") {
+        REQUIRE(true);
+    }
 }
 
-TEST_CASE("case2")
-{
-   SECTION("function2")
-   {
-      REQUIRE(false);
-   }
+TEST_CASE("case2") {
+    SECTION("function2") {
+        REQUIRE(false);
+    }
 }
