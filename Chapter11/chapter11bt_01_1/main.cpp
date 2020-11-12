74d9eb125554168fae45b80a04edf7385fc47006
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2020-11-12 20:27:54 +0800

    Add cmake files

--- Chapter11/chapter11bt_01_1/main.cpp
+++ Chapter11/chapter11bt_01_1/main.cpp
@@ -1,8 +1,6 @@
 #define BOOST_TEST_MODULE My first test module
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(first_test_function)
-{
-   BOOST_TEST(true);
+BOOST_AUTO_TEST_CASE(first_test_function) {
+    BOOST_TEST(true);
 }
-
